9ab045a315e70c1b16f31c6e9a514d2a6664a942
kada2004 <dankalenga3@gmail,com>
2025-11-13 20:51:29 +0100

    Lets go

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,6 +1,6 @@
 resource "azurerm_resource_group" "rg" {
   name     = "rg-${local.project_prefix}-${local.project_name}"
-  tags     = local.tags
+  #tags     = local.tags
   location = var.location
 
   lifecycle {
