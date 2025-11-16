93a8ec5b099ab1bb25044cc5090fbae5c3a32ee0
kada2004 <dankalenga3@gmail,com>
2025-11-16 18:10:23 +0100

    Lets go

--- Terraform/key_vault.tf
+++ Terraform/key_vault.tf
@@ -12,15 +12,3 @@ resource "azurerm_key_vault" "streaming_time_SeriesIot" {
   }
 }
 
-# Function App access policy
-
-resource "azurerm_key_vault_access_policy" "function_app01" {
-  key_vault_id = azurerm_key_vault.streaming_time_SeriesIot.id
-  tenant_id    = azurerm_linux_function_app.function_app_ingestion01.identity[0].tenant_id
-  object_id    = azurerm_linux_function_app.function_app_ingestion01.identity[0].principal_id
-
-  secret_permissions = [
-    "Get",
-    "List"
-  ]
-}
