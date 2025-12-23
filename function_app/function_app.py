e63b95ebac18a67ae415ca61eed57662306eeb4b
kada2004 <dankalenga3@gmail.com>
2025-12-23 09:47:49 +0100

    Deploy Function

--- function_app/function_app.py
+++ function_app/function_app.py
@@ -6,12 +6,11 @@ from azure.keyvault.secrets import SecretClient
 
 app = func.FunctionApp()
 
-
+@app.function_name(name="fetchweatherapi")
 @app.timer_trigger(
     schedule="0 * * * * *",
     arg_name="myTimer",
-    run_on_startup=False,
-    use_monitor=False
+    run_on_startup=False
 )
 def fetchweatherapi(myTimer: func.TimerRequest) -> None:
     if myTimer.past_due:
@@ -35,5 +34,3 @@ def fetchweatherapi(myTimer: func.TimerRequest) -> None:
         logging.info(f"Weather data for {city}: {data}")
     except Exception as e:
         logging.error(f"Error fetching weather data: {e}")
-        
-        
