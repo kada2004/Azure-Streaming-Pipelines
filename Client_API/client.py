e56c3079dee9f61e93d75234d49b732f20bd0bc1
kada2004 <dankalenga3@gmail,com>
2025-12-30 06:04:33 +0100

    re-factor client

--- Client_API/client.py
+++ Client_API/client.py
@@ -4,59 +4,87 @@ import json
 import linecache
 import requests
 import time
+import hashlib
+from pathlib import Path
+from datetime import datetime, timezone
 
 
-# CONFIG
+# Config
 
 KEYVAULT_URL = "https://kv-Az-TimeSeries.vault.azure.net/"
 SECRET_NAME = "apim-subscription-key"
 API_URL = "https://apim-az-management.azure-api.net/ingest"
 
-#SUBSCRIPTION_KEY = ""
-
-# Authenticate
-credential = DefaultAzureCredential()
-client = SecretClient(vault_url=KEYVAULT_URL, credential=credential)
-
-# Get secret
-SUBSCRIPTION_KEY = client.get_secret(SECRET_NAME).value
 INPUT_FILE = "./output10lines.txt"
-
 START_LINE = 1
 END_LINE = 10
+
+# AUTH
+
+
+credential = DefaultAzureCredential()
+client = SecretClient(vault_url=KEYVAULT_URL, credential=credential)
+SUBSCRIPTION_KEY = client.get_secret(SECRET_NAME).value
 
 HEADERS = {
     "Content-Type": "application/json",
     "Ocp-Apim-Subscription-Key": SUBSCRIPTION_KEY
 }
 
+file_name = Path(INPUT_FILE).name
 
-# SEND LOOP
 
-for i in range(START_LINE, END_LINE + 1):
-    line = linecache.getline(INPUT_FILE, i)
+for line_number in range(START_LINE, END_LINE + 1):
+
+    line = linecache.getline(INPUT_FILE, line_number)
 
     if not line.strip():
-        print(f"Line {i} is empty, skipping")
+        print(f"Line {line_number} is empty, skipping")
         continue
 
     try:
         payload = json.loads(line)
     except json.JSONDecodeError as e:
-        print(f"Invalid JSON at line {i}: {e}")
+        print(f"Invalid JSON at line {line_number}: {e}")
         continue
 
-    print(f"Sending line {i}: {payload}")
+    # stable JSON 
+    canonical_payload = json.dumps(
+        payload,
+        sort_keys=True,
+        separators=(",", ":")
+    )
+
+    # event_time
+    event_time = payload.get(
+        "event_time",
+        datetime.now(timezone.utc).isoformat()
+    )
+
+    # event_id 
+    raw_id = f"{file_name}|line={line_number}|{canonical_payload}"
+    event_id = hashlib.sha256(raw_id.encode("utf-8")).hexdigest()
+
+    #  message 
+    message = {
+        "event_id": event_id,
+        "event_time": event_time,
+        "ingested_at": datetime.now(timezone.utc).isoformat(),
+        "source_file": file_name,
+        "source_line": line_number,
+        "payload": payload
+    }
+
+    print(f"Sending line {line_number} | event_id={event_id}")
 
     response = requests.post(
         API_URL,
         headers=HEADERS,
-        json=payload,
+        json=message,
         timeout=10
     )
 
     print(f"Status: {response.status_code}")
     print(f"Response: {response.text}")
 
-    
     time.sleep(0.05)
