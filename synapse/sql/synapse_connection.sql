29f0a94236e9f218ec48413c0a1bef460c8bb592
kada2004 <dankalenga3@gmail.com>
2026-01-03 13:35:26 +0100

    removing one time connection

--- synapse/sql/synapse_connection.sql
+++ synapse/sql/synapse_connection.sql
@@ -1,3 +1,3 @@
 --- one time 
-CREATE DATABASE SCOPED CREDENTIAL SynapseMI
-WITH IDENTITY = 'Managed Identity';
+---CREATE DATABASE SCOPED CREDENTIAL SynapseMI
+---WITH IDENTITY = 'Managed Identity';
