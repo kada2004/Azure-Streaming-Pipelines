310336ef48121aed8fc600b2af82f7c0579d134d
kada2004 <dankalenga3@gmail.com>
2026-01-03 14:45:18 +0100

    lets goo again 🤣😂😂😂

--- synapse/sql/iot_adls.sql
+++ synapse/sql/iot_adls.sql
@@ -1,5 +1,12 @@
-CREATE EXTERNAL DATA SOURCE IOT_ADLS
-WITH (
-    LOCATION = 'https://stdatastorequality.dfs.core.windows.net/iot-stream',
-    CREDENTIAL = SynapseMI
-);
+IF NOT EXISTS (
+    SELECT 1
+    FROM sys.external_data_sources
+    WHERE name = 'IOT_ADLS'
+)
+BEGIN
+    CREATE EXTERNAL DATA SOURCE IOT_ADLS
+    WITH (
+        LOCATION = 'https://stdatastorequality.dfs.core.windows.net/iot-stream',
+        CREDENTIAL = SynapseMI
+    );
+END;
