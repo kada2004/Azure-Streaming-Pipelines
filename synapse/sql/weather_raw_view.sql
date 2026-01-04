931a398b52c6b201f4123e496ff29d596347564c
kada2004 <dankalenga3@gmail.com>
2026-01-04 19:45:22 +0100

    update view syntax

--- synapse/sql/weather_raw_view.sql
+++ synapse/sql/weather_raw_view.sql
@@ -1,24 +1,23 @@
-CREATE OR ALTER VIEW vw_weather_raw
+IF OBJECT_ID('dbo.vw_weather_raw', 'V') IS NOT NULL
+BEGIN
+    EXEC('DROP VIEW dbo.vw_weather_raw');
+END;
+GO
+
+CREATE VIEW dbo.vw_weather_raw
 AS
 SELECT
-    JSON_VALUE(doc, '$.event_id')                        AS event_id,
-    CAST(JSON_VALUE(doc, '$.event_time') AS datetime2)   AS event_time,
-    JSON_VALUE(doc, '$.payload.name')                    AS city_name,
-    JSON_VALUE(doc, '$.payload.sys.country')             AS country,
-    CAST(JSON_VALUE(doc, '$.payload.coord.lat') AS decimal(9,6)) AS latitude,
-    CAST(JSON_VALUE(doc, '$.payload.coord.lon') AS decimal(9,6)) AS longitude,
-    JSON_VALUE(doc, '$.payload.weather[0].main')         AS weather_main,
-    JSON_VALUE(doc, '$.payload.weather[0].description')  AS weather_description,
-    CAST(JSON_VALUE(doc, '$.payload.main.temp') AS decimal(5,2)) - 273.15 AS temperature_c,
-    CAST(JSON_VALUE(doc, '$.payload.main.humidity') AS int) AS humidity,
-    CAST(JSON_VALUE(doc, '$.payload.main.pressure') AS int) AS pressure,
-    CAST(JSON_VALUE(doc, '$.payload.wind.speed') AS decimal(5,2)) AS wind_speed,
-    CAST(JSON_VALUE(doc, '$.payload.timezone') AS int)    AS timezone
-FROM
-    OPENROWSET(
-        BULK 'WEATHER/*.json',
-        DATA_SOURCE = 'WEATHER_ADLS',
-        FORMAT = 'CSV',
-        FIELDTERMINATOR = '0x0b',
-        FIELDQUOTE = '0x0b'
-    ) WITH (doc varchar(max)) AS rows;
+    JSON_VALUE(raw_json, '$.event_id') AS event_id,
+    CAST(JSON_VALUE(raw_json, '$.event_time') AS datetime2) AS event_time,
+    JSON_VALUE(raw_json, '$.payload.name') AS city_name,
+    JSON_VALUE(raw_json, '$.payload.sys.country') AS country,
+    CAST(JSON_VALUE(raw_json, '$.payload.coord.lat') AS decimal(9,6)) AS latitude,
+    CAST(JSON_VALUE(raw_json, '$.payload.coord.lon') AS decimal(9,6)) AS longitude,
+    JSON_VALUE(raw_json, '$.payload.weather[0].main') AS weather_main,
+    JSON_VALUE(raw_json, '$.payload.weather[0].description') AS weather_description,
+    CAST(JSON_VALUE(raw_json, '$.payload.main.temp') AS decimal(6,2)) - 273.15 AS temperature_c,
+    CAST(JSON_VALUE(raw_json, '$.payload.main.humidity') AS int) AS humidity,
+    CAST(JSON_VALUE(raw_json, '$.payload.main.pressure') AS int) AS pressure,
+    CAST(JSON_VALUE(raw_json, '$.payload.wind.speed') AS decimal(5,2)) AS wind_speed,
+    CAST(JSON_VALUE(raw_json, '$.payload.timezone') AS int) AS timezone
+FROM dbo.weather_raw_ext;
